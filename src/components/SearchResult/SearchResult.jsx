4636db6850b08493ab6780b4295c8bbc095b8f64
Sabella-8 <gsabella549@gmail.com>
2024-03-13 01:15:08 +0000

    fetch booking

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -1,35 +1,44 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./SearchResult.scss";
-import FakeBookings from "@/data/fakeBookings.json";
+// import FakeBookings from "@/data/fakeBookings.json";
 import TableBody from "./TableBody";
 import TableHead from "./TableHead";
 import dayjs from "dayjs";
 
-const Bookings = FakeBookings;
+// const Bookings = FakeBookings;
 function SearchResult() {
+  const [fakeBookings, setFakeBookings] = useState([]);
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((res) => res.json())
+      .then((data) => setFakeBookings(data));
+  }, []);
+  console.log(fakeBookings);
   return (
     <table>
       <thead>
         <TableHead />
       </thead>
       <tbody>
-        {Bookings.map((book) => {
+        {fakeBookings.map((book) => {
           const stayNightsTotal = dayjs(book.checkOutDate).diff(
             dayjs(book.checkInDate),
             "day"
           );
           return (
-              <TableBody
-                key={book.id}
-                id={book.id}
-                title={book.title}
-                firstName={book.firstName}
-                surName={book.surname}
-                email={book.email}
-                roomId={book.roomId}
-                checkInDate={book.checkInDate}
-                checkOutDate={book.checkOutDate}
-                stayNights={stayNightsTotal}
-              />
+            <TableBody
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              firstName={book.firstName}
+              surName={book.surname}
+              email={book.email}
+              roomId={book.roomId}
+              checkInDate={book.checkInDate}
+              checkOutDate={book.checkOutDate}
+              stayNights={stayNightsTotal}
+            />
           );
         })}
       </tbody>
@@ -37,4 +46,3 @@ function SearchResult() {
   );
 }
 export default SearchResult;
-
