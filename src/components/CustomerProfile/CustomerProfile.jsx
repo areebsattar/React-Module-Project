ec10d627e8e03a8b3313aafadc9d06a670059775
areebsattar <areebsattar1@gmail.com>
2024-03-17 21:34:54 +0000

    Implemented customerProfileFetch

--- src/components/CustomerProfile/CustomerProfile.jsx
+++ src/components/CustomerProfile/CustomerProfile.jsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
+function CustomerProfile({ id }) {
+  const [customerProfileData, setCustomerProfileData] = useState(null);
 
-function CustomerProfile(props) {
-    return <tr>`Customer {props.id} profile`</tr>;
+  useEffect(() => {
+    fetch(`https://cyf-hotel-api.netlify.app/customers/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCustomerProfileData(data))
+      .catch((error) =>
+        console.error("Error fetching customer profile:", error)
+      );
+  }, [id]);
 
+  if (!customerProfileData) {
+    return (
+      <tr className="customer-row">
+        <td colSpan={2}>Loading...</td>
+      </tr>
+    );
+  }
+  return (
+    <tr className="customer-row">
+      <td colSpan={2}>{customerProfileData.phoneNumber}</td>
+    </tr>
+  );
 }
 
-export default CustomerProfile
+export default CustomerProfile;
