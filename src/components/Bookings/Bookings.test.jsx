4b91b35fb4b4de79883ef48fdfb40091e0f32212
areebsattar <areebsattar1@gmail.com>
2024-03-29 01:56:28 +0000

    Test completed for the component as well

--- src/components/Bookings/Bookings.test.jsx
+++ src/components/Bookings/Bookings.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Bookings from "./Bookings.jsx";
 
 describe("Bookings Component", () => {
@@ -7,5 +7,25 @@ describe("Bookings Component", () => {
     render(<Bookings />);
     const mainElement = screen.getByRole("main");
     expect(mainElement).toBeInTheDocument();
+  });
+
+  it("displays the loading message initially", () => {
+    render(<Bookings />);
+    const loadingMessage = screen.getByText("Loading Information Please Wait...");
+    expect(loadingMessage).toBeInTheDocument();
+  });
+
+  it("hides the loading message when booking data is rendered", async () => {
+    global.fetch = () =>
+      Promise.resolve({
+        ok: true,
+        json: async() => [{ id: 1, name: "John Doe" }] ,
+      });
+    
+    render(<Bookings />);
+    await waitFor(() => {
+      const loadingMessage = screen.queryByText('Loading Information Please Wait...');
+      expect(loadingMessage).not.toBeInTheDocument();
+    });
   });
 });
