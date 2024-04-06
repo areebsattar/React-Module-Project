814a41595728ff9b3086a94338db12e3d2f4ef22
areebsattar <areebsattar1@gmail.com>
2024-04-06 14:57:32 +0100

    Solved the problem with extra import lines in seacrh result

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -4,11 +4,6 @@ import CustomerProfile from "../CustomerProfile/CustomerProfile";
 import Table from "./Table";
 
 
-function SearchResult() {
-
-import TableBody from "./TableBody";
-import TableHead from "./TableHead";
-import dayjs from "dayjs";
 
 function SearchResult(props) {
   const Bookings = props.results;
