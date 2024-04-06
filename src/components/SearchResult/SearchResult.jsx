da802ac4fae45ebc2e5376ee0890942ae7718a23
areebsattar <areebsattar1@gmail.com>
2024-04-06 13:35:58 +0100

    Revert "NW6 | Orlando Morales | BookingForms-ReactProject | Week3"

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./SearchResult.scss";
 
 import CustomerProfile from "../CustomerProfile/CustomerProfile";
@@ -11,37 +10,6 @@ import TableBody from "./TableBody";
 import TableHead from "./TableHead";
 import dayjs from "dayjs";
 
-
-function SearchResult({ results}) {
-  return (
-    <table>
-      <thead>
-        <TableHead />
-      </thead>
-      <tbody>
-        {bookings.map((book) => {
-          const stayNightsTotal = dayjs(book.checkOutDate).diff(
-            dayjs(book.checkInDate),
-            "day"
-          );
-          return (
-            <TableBody
-              key={book.id}
-              id={book.id}
-              title={book.title}
-              firstName={book.firstName}
-              surName={book.surname}
-              email={book.email}
-              roomId={book.roomId}
-              checkInDate={book.checkInDate}
-              checkOutDate={book.checkOutDate}
-              stayNights={stayNightsTotal}
-            />
-          );
-        })}
-      </tbody>
-    </table>
-
 function SearchResult(props) {
   const Bookings = props.results;
 
@@ -50,7 +18,6 @@ function SearchResult(props) {
       <Table />
       <CustomerProfile />
     </>
-
   );
 }
 export default SearchResult;
